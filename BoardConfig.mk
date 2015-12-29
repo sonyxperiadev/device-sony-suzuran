0efb80088ad17dd7ed439c9a2005831e53ee6bd7
David Viteri <davidteri91@gmail.com>
2015-12-29 19:38:10 +0100

    suzuran: Enable simple PowerHAL for native double-tap-to-wake
    Signed-off-by: Adam Farden <adam@farden.cz>
    Signed-off-by: David Viteri <davidteri91@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,3 +16,6 @@ include device/sony/kitakami/BoardConfig.mk
 include device/sony/common/CommonConfig.mk
 
 TARGET_BOOTLOADER_BOARD_NAME := E5823
+
+TARGET_TAP_TO_WAKE_NODE := "/sys/devices/virtual/input/clearpad/wakeup_gesture"
+TARGET_TAP_TO_WAKE_STRING := true
