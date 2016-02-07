bdd114ae05cfe1daf27065b4f697001f086d1201
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2016-02-07 18:59:07 +0100

    use aosp_base_telephony
    follow aosp and use aosp_base_telephony on devices that have GSM
    
    this switch allowes also some package cleanup
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>

--- aosp_e5823.mk
+++ aosp_e5823.mk
@@ -17,7 +17,7 @@ TARGET_KERNEL_CONFIG := aosp_kitakami_suzuran_defconfig
 DEVICE_PACKAGE_OVERLAYS += \
     device/sony/suzuran/overlay
 
-$(call inherit-product, $(SRC_TARGET_DIR)/product/telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 $(call inherit-product, device/sony/kitakami/device.mk)
 $(call inherit-product, vendor/sony/suzuran/suzuran-vendor.mk)
 $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
