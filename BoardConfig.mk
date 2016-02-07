daef3dbda71ae0d9fd3721dcf7df743a7f515f24
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2016-02-07 19:58:55 +0100

    fix platform naming
    in initial commit platform was named Board and device which
    is confusing. This commit fixes the naming making things easy
    to understand
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-include device/sony/kitakami/BoardConfig.mk
+include device/sony/kitakami/PlatformConfig.mk
 
 TARGET_BOOTLOADER_BOARD_NAME := E5823
 
