4001c63e9ac9b5baabfbc135160a03532e5c1667
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2016-02-09 20:01:32 +0100

    device rework
    We split device from aosp_device so that it can be included
    easy in custom projects.
    
    New projects should create a new target named <project>_device
    which contains project specific configuration and options.
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>

---
+++ device.mk
@@ -0,0 +1,56 @@
+# Copyright 2014 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+DEVICE_PACKAGE_OVERLAYS += \
+    device/sony/suzuran/overlay
+
+PRODUCT_COPY_FILES := \
+    device/sony/suzuran/rootdir/system/etc/audio_policy.conf:system/etc/audio_policy.conf \
+    device/sony/suzuran/rootdir/system/etc/BCM4356.hcd:system/etc/firmware/BCM43xx.hcd \
+    device/sony/suzuran/rootdir/system/etc/wifi/bcmdhd.cal:system/etc/wifi/bcmdhd.cal \
+    device/sony/suzuran/rootdir/system/etc/sensor_def_qcomdev.conf:system/etc/sensor_def_qcomdev.conf \
+    device/sony/suzuran/rootdir/system/etc/thermanager.xml:system/etc/thermanager.xml \
+    device/sony/suzuran/rootdir/system/etc/libnfc-brcm.conf:system/etc/libnfc-brcm.conf \
+    device/sony/suzuran/rootdir/system/etc/libnfc-nxp.conf:system/etc/libnfc-nxp.conf \
+    device/sony/suzuran/rootdir/system/etc/mixer_paths.xml:system/etc/mixer_paths.xml
+
+# Device Init
+PRODUCT_PACKAGES += \
+    init.recovery.suzuran \
+    init.suzuran \
+    ueventd.suzuran
+
+# Lights
+PRODUCT_PACKAGES += \
+    lights.suzuran
+
+# Simple PowerHAL
+PRODUCT_PACKAGES += \
+    power.suzuran
+
+# NFC config
+PRODUCT_PACKAGES += \
+    nfc_nci.pn54x.default
+
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREBUILT_DPI := xhdpi hdpi
+PRODUCT_AAPT_PREF_CONFIG := xhdpi
+
+PRODUCT_PROPERTY_OVERRIDES := \
+    ro.sf.lcd_density=320 \
+    ro.usb.pid_suffix=1DA
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, device/sony/kitakami/platform.mk)
+$(call inherit-product, vendor/sony/suzuran/suzuran-vendor.mk)
