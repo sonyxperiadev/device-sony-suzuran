db58dee5eb7cb31455c2908c7d379e1c5ab21609
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2016-04-05 14:21:02 +0200

    rename vendor/sony/device
    group vendor devices by platform for easy tracking
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>

--- device.mk
+++ device.mk
@@ -61,4 +61,4 @@ PRODUCT_PROPERTY_OVERRIDES := \
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, device/sony/kitakami/platform.mk)
-$(call inherit-product, vendor/sony/suzuran/suzuran-vendor.mk)
+$(call inherit-product, vendor/sony/kitakami-suzuran/suzuran-vendor.mk)
